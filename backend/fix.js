831540f46a69854f94b633d8c18fabd0807406b1
Celixwk <celixwk@users.noreply.github.com>
2026-09-01 21:45:06 -0500

    chore: remove temporary build scripts

--- backend/fix.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let s = fs.readFileSync('prisma/schema.sql', 'utf8');
-s = s.replace(/\\"/g, '"');
-fs.writeFileSync('prisma/schema.sql', s);
--- backend/patch.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let s = fs.readFileSync('prisma/schema.sql', 'utf8');
-s = s.replace(/CREATE TABLE "/g, 'CREATE TABLE IF NOT EXISTS "');
-s = s.replace(/CREATE INDEX "/g, 'CREATE INDEX IF NOT EXISTS "');
-s = s.replace(/CREATE UNIQUE INDEX "/g, 'CREATE UNIQUE INDEX IF NOT EXISTS "');
-fs.writeFileSync('prisma/schema.sql', s);
